e2b607d778a1e400c5d5e9f18f1327870664b037
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2022-12-20 10:42:13 -0500

    Updates to grab rest of tweets

--- database/import-from-archive.js
+++ database/import-from-archive.js
@@ -1,18 +1,28 @@
-require('dotenv').config();
-const { checkInDatabase, logTweetCount, saveToDatabaseApiV1, createTable } = require("./tweet-to-db");
-const shouldFilterOutCircleTweets = process.argv.includes('removecircletweets');
-const tweets = require("./tweets.js");
+require("dotenv").config();
+const {
+	checkInDatabase,
+	logTweetCount,
+	saveToDatabaseApiV1,
+	createTable,
+} = require("./tweet-to-db");
+const shouldFilterOutCircleTweets = process.argv.includes("removecircletweets");
+const twitterPart0 = require("./tweets.js");
+const twitterPart1 = require("./tweets-part1.js");
+const twitterPart2 = require("./tweets-part2.js");
+const tweets = [...twitterPart0, ...twitterPart1, ...twitterPart2];
 let circleTweets;
 
 if (shouldFilterOutCircleTweets) {
 	circleTweets = require("./twitter-circle-tweet.js");
 }
 
-console.log( `${tweets.length} tweets found in archive.` );
+console.log(`${tweets.length} tweets found in archive.`);
 logTweetCount();
 
 function tweetIsForCircles(tweet) {
-	return circleTweets.some(circleTweet => circleTweet.tweet.id_str === tweet.id_str);
+	return circleTweets.some(
+		(circleTweet) => circleTweet.tweet.id_str === tweet.id_str
+	);
 }
 
 async function retrieveTweets() {
@@ -20,31 +30,33 @@ async function retrieveTweets() {
 	let missingTweets = 0;
 	let circleTweets = 0;
 
-	for(let {tweet} of tweets ) {
+	for (let { tweet } of tweets) {
 		checkInDatabase(tweet).then((tweet) => {
-			if(tweet === false) {
+			if (tweet === false) {
 				existingRecordsFound++;
-			} else if (shouldFilterOutCircleTweets && tweetIsForCircles(tweet)) {
+			} else if (
+				shouldFilterOutCircleTweets &&
+				tweetIsForCircles(tweet)
+			) {
 				circleTweets++;
-				console.log( {circleTweets} );
+				console.log({ circleTweets });
 			} else {
 				missingTweets++;
 				saveToDatabaseApiV1(tweet);
 				// console.log( "Missing tweet", { tweet });
-				console.log( {existingRecordsFound, missingTweets} );
+				console.log({ existingRecordsFound, missingTweets });
 				logTweetCount();
 			}
 		});
 	}
 }
 
-(async function() {
+(async function () {
 	try {
 		createTable();
 
 		await retrieveTweets();
-	} catch(e) {
-		console.log( "ERROR", e );
+	} catch (e) {
+		console.log("ERROR", e);
 	}
 })();
-
